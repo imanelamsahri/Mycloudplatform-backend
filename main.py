53e117899b68af361f5bbd3155d77284fb365530
imanelamsahri <lamsahriimane@iav.ac.ma>
2025-04-02 00:37:08 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
+
+app = FastAPI()
+
+# Autoriser les requêtes depuis le frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Bienvenue sur l'API Mycloudplatform 🎉"}
+
+@app.get("/analyser")
+def analyser(produit: Optional[str] = "pomme", rayon: Optional[float] = 10):
+    return {
+        "produit": produit,
+        "rayon": rayon,
+        "geojson": {
+            "type": "FeatureCollection",
+            "features": []
+        }
+    }
